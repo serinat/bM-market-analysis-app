20736659caeac26ae04573a4f4980c0ffd9bebf9
Serina Tsang <serinat@uw.edu>
2016-02-10 23:50:45 -0800

    start over

--- app.js
+++ app.js
@@ -6,16 +6,3 @@ function Image(name) {
   this.path = "media/" + name + ".jpg";
   this.votes = 0;
 };
-
-var Tracker = {
-  leftEl: document.getElementById("leftChoice"),
-  centerEl: document.getElementById("centerChoice"),
-  rightEl: document.getElementById("rightChoice"),
-  numRandomizer: function () {
-    return.Mathfloor(Math.random( * (images.length -1));
-  },
-  pickImages: function() {
-    var img
-
-
-}
